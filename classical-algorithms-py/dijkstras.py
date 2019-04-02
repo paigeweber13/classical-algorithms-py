766a59adb9a55c2c2705b74bbc90c3ccc876de61
Riley Weber <rileyw13@protonmail.com>
2019-04-02 00:00:05 -0400

    added description

--- classical-algorithms-py/dijkstras.py
+++ classical-algorithms-py/dijkstras.py
@@ -1,2 +1,19 @@
+"""
+works on a "graph" that is implemented using python dicts, as shown below.
+example_graph = { "a" : ["b","c"],
+                  "b" : ["a", "d"],
+                  "c" : ["a", "d"],
+                  "d" : ["e"],
+                  "e" : ["d"]
+                }
+
+diagram of example_graph:
+
+    a --- b
+    |     |
+    |     |
+    c --- d --- e
+"""
+
 def dijkstras():
     pass
